454630392336944e89cc7f20fedae851c9cfaac8
Saivarma Atti <92287589+saivarmaatti03@users.noreply.github.com>
2022-07-02 09:48:31 +0530

    #include <stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",i+64);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,n,x;
+    int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
             printf("%c ",i+64);
         }
